77d1fa195db37de0c73fbfd6306aea0b9455b700
Michael Saverino <MAS1273@student.phsc.edu>
2023-11-04 16:26:28 -0400

    Added Documentation class.

---
+++ SaveClipboardScreenshot/Classes/Documentation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveClipboardScreenshot.Classes
+{
+    internal class Documentation
+    {
+        private const string _configurationUrlDev = "https://github.com/msaverino/SaveClipboardScreenshot/blob/development/SaveClipboardScreenshot/docs/Configuration.md";
+        private const string _configurationUrl = "https://github.com/msaverino/SaveClipboardScreenshot/blob/main/SaveClipboardScreenshot/docs/Configuration.md";
+
+        internal static void OpenConfigurationPage()
+        {
+            if (IsRelease())
+                System.Diagnostics.Process.Start(_configurationUrl);
+            else
+                System.Diagnostics.Process.Start(_configurationUrlDev);
+        }
+
+        internal static bool IsRelease()
+        {
+            #if DEBUG
+                return false;
+            #else
+                return true;
+            #endif
+        }
+    }
+}
