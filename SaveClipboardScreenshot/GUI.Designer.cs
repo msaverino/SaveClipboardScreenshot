5e125e2ca8690eb1f8de7558b051e23214eb5003
Michael Saverino <MAS1273@student.phsc.edu>
2023-11-04 16:28:52 -0400

    Created Settings tab.

--- SaveClipboardScreenshot/GUI.Designer.cs
+++ SaveClipboardScreenshot/GUI.Designer.cs
@@ -31,11 +31,11 @@
             this.TabControl = new System.Windows.Forms.TabControl();
             this.Action_Tab = new System.Windows.Forms.TabPage();
             this.Settings_Tab = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Button_SelectDirectory = new System.Windows.Forms.Button();
+            this.ComboBox_Suffix = new System.Windows.Forms.ComboBox();
+            this.TextBox_Directory = new System.Windows.Forms.TextBox();
             this.Label_Suffix = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Label_SaveDirectory = new System.Windows.Forms.Label();
             this.TabControl.SuspendLayout();
             this.Settings_Tab.SuspendLayout();
             this.SuspendLayout();
@@ -47,7 +47,7 @@
             this.TabControl.Location = new System.Drawing.Point(12, 12);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(465, 206);
+            this.TabControl.Size = new System.Drawing.Size(465, 92);
             this.TabControl.TabIndex = 0;
             // 
             // Action_Tab
@@ -55,49 +55,49 @@
             this.Action_Tab.Location = new System.Drawing.Point(4, 22);
             this.Action_Tab.Name = "Action_Tab";
             this.Action_Tab.Padding = new System.Windows.Forms.Padding(3);
-            this.Action_Tab.Size = new System.Drawing.Size(457, 180);
+            this.Action_Tab.Size = new System.Drawing.Size(457, 66);
             this.Action_Tab.TabIndex = 0;
             this.Action_Tab.Text = "Action";
             this.Action_Tab.UseVisualStyleBackColor = true;
             // 
             // Settings_Tab
             // 
-            this.Settings_Tab.Controls.Add(this.button1);
-            this.Settings_Tab.Controls.Add(this.comboBox1);
-            this.Settings_Tab.Controls.Add(this.textBox1);
+            this.Settings_Tab.Controls.Add(this.Button_SelectDirectory);
+            this.Settings_Tab.Controls.Add(this.ComboBox_Suffix);
+            this.Settings_Tab.Controls.Add(this.TextBox_Directory);
             this.Settings_Tab.Controls.Add(this.Label_Suffix);
-            this.Settings_Tab.Controls.Add(this.label1);
+            this.Settings_Tab.Controls.Add(this.Label_SaveDirectory);
             this.Settings_Tab.Location = new System.Drawing.Point(4, 22);
             this.Settings_Tab.Name = "Settings_Tab";
             this.Settings_Tab.Padding = new System.Windows.Forms.Padding(3);
-            this.Settings_Tab.Size = new System.Drawing.Size(457, 180);
+            this.Settings_Tab.Size = new System.Drawing.Size(457, 66);
             this.Settings_Tab.TabIndex = 1;
             this.Settings_Tab.Text = "Settings";
             this.Settings_Tab.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // Button_SelectDirectory
             // 
-            this.button1.Location = new System.Drawing.Point(345, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Button_SelectDirectory.Location = new System.Drawing.Point(345, 30);
+            this.Button_SelectDirectory.Name = "Button_SelectDirectory";
+            this.Button_SelectDirectory.Size = new System.Drawing.Size(106, 23);
+            this.Button_SelectDirectory.TabIndex = 4;
+            this.Button_SelectDirectory.Text = "Select Directory";
+            this.Button_SelectDirectory.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // ComboBox_Suffix
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(89, 30);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(250, 21);
-            this.comboBox1.TabIndex = 3;
+            this.ComboBox_Suffix.FormattingEnabled = true;
+            this.ComboBox_Suffix.Location = new System.Drawing.Point(89, 30);
+            this.ComboBox_Suffix.Name = "ComboBox_Suffix";
+            this.ComboBox_Suffix.Size = new System.Drawing.Size(250, 21);
+            this.ComboBox_Suffix.TabIndex = 3;
             // 
-            // textBox1
+            // TextBox_Directory
             // 
-            this.textBox1.Location = new System.Drawing.Point(89, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(362, 20);
-            this.textBox1.TabIndex = 2;
+            this.TextBox_Directory.Location = new System.Drawing.Point(89, 6);
+            this.TextBox_Directory.Name = "TextBox_Directory";
+            this.TextBox_Directory.Size = new System.Drawing.Size(362, 20);
+            this.TextBox_Directory.TabIndex = 2;
             // 
             // Label_Suffix
             // 
@@ -109,20 +109,20 @@
             this.Label_Suffix.Text = "Suffix:";
             this.Label_Suffix.DoubleClick += new System.EventHandler(this.Label_Suffix_DoubleClick);
             // 
-            // label1
+            // Label_SaveDirectory
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Save Directory:";
+            this.Label_SaveDirectory.AutoSize = true;
+            this.Label_SaveDirectory.Location = new System.Drawing.Point(3, 9);
+            this.Label_SaveDirectory.Name = "Label_SaveDirectory";
+            this.Label_SaveDirectory.Size = new System.Drawing.Size(80, 13);
+            this.Label_SaveDirectory.TabIndex = 0;
+            this.Label_SaveDirectory.Text = "Save Directory:";
             // 
             // ClipboardToScreenshot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 230);
+            this.ClientSize = new System.Drawing.Size(489, 113);
             this.Controls.Add(this.TabControl);
             this.Name = "ClipboardToScreenshot";
             this.Text = "Clipboard to Screenshot";
@@ -138,11 +138,11 @@
         private System.Windows.Forms.TabControl TabControl;
         private System.Windows.Forms.TabPage Action_Tab;
         private System.Windows.Forms.TabPage Settings_Tab;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Button_SelectDirectory;
+        private System.Windows.Forms.ComboBox ComboBox_Suffix;
+        private System.Windows.Forms.TextBox TextBox_Directory;
         private System.Windows.Forms.Label Label_Suffix;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label_SaveDirectory;
     }
 }
 
