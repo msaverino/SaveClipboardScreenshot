e8472fde246271df9f4f1587e8b203784ab3b794
Michael Saverino <MAS1273@student.phsc.edu>
2023-11-04 16:33:15 -0400

    Created the Model that will be used within the application.

---
+++ SaveClipboardScreenshot/Models/ConfigurationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveClipboardScreenshot.Models
+{
+    public class ConfigurationModel
+    {
+        public string Suffix { get; set; }
+        public string Folder { get; set; }
+        public bool CreateChildFolder { get; set; }
+    }
+}
